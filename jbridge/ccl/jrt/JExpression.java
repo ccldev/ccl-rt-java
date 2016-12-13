4e0d0048438107443783943fe9bc5ab791a3a5f1
NetLamp <oplpublishergerman@gmail.com>
2016-12-13 12:31:27 +0100

    Inner Class support

--- jbridge/ccl/jrt/JExpression.java
+++ jbridge/ccl/jrt/JExpression.java
@@ -3,6 +3,7 @@ package ccl.jrt;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import ccl.rt.Expression;
 import ccl.rt.Special;
@@ -13,17 +14,27 @@ public class JExpression extends Expression {
 
 	private Object object;
 	private Method[] methods;
+	private JClass innerClass;
 	private Class<?> clss;
 	
 	public JExpression(Object o, Class<?> c, String name) {
 		super(Special.INVALID);
+		
 		this.object = o;
 		this.clss = c;
 		Method[] m = getMethods(name);
 		Field f = getField(name);
 		this.methods = m;
 
-		if (m.length == 0 && f == null) {
+		Class<?>[] classes = c.getDeclaredClasses();
+		for(int i = 0; i < classes.length; i++){
+			if(classes[i].getSimpleName().equals(name)){
+				innerClass = new JClass(classes[i]);
+				break;
+			}
+		}
+		
+		if (m.length == 0 && f == null && innerClass == null) {
 			setValue(new Err(new RuntimeException("No such native property '"
 					+ name + "' on Object " + o)));
 			return;
@@ -61,10 +72,21 @@ public class JExpression extends Expression {
 	}
 
 	public Value invoke(Value... args) throws Exception {
+		if(innerClass != null){
+			return innerClass.invoke(args);
+		}
 		if (methods.length == 0) {
 			return super.invoke(args);
 		} else {
 			return J.invoke(object, Call.pack(methods), args);
+		}
+	}
+	
+	public Value getProperty(String s){
+		if(innerClass != null){
+			return innerClass.getProperty(s);
+		}else{
+			return super.getProperty(s);
 		}
 	}
 
